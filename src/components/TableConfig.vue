4dcec3e5671416cdd12a1ef839e4630b597d308b
Heri Dev <hheri58@gmail.com>
2026-08-07 16:22:36 +0700

    fix(ui): expand layout and table grid to 100% full screen width

--- src/components/TableConfig.vue
+++ src/components/TableConfig.vue
@@ -326,7 +326,7 @@ const deleteTableTemplate = () => {
 
 .table-checkbox-grid {
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
   gap: 8px;
 }
 
