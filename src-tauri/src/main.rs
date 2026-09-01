6b899c03fe22605bda55550dbbe75c5884ece1da
Heri Dev <hheri58@gmail.com>
2026-09-01 11:07:40 +0700

    refactor: comment out linux-specific webkit environment variable overrides in main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,15 +2,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    #[cfg(target_os = "linux")]
-    {
-        if std::env::var("WEBKIT_FORCE_COMPOSITING_MODE").is_err() {
-            std::env::set_var("WEBKIT_FORCE_COMPOSITING_MODE", "1");
-        }
-        if std::env::var("WEBKIT_DISABLE_DMABUF_RENDERER").is_err() {
-            std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
-        }
-    }
+    // #[cfg(target_os = "linux")]
+    // {
+    //     if std::env::var("WEBKIT_FORCE_COMPOSITING_MODE").is_err() {
+    //         std::env::set_var("WEBKIT_FORCE_COMPOSITING_MODE", "1");
+    //     }
+    //     if std::env::var("WEBKIT_DISABLE_DMABUF_RENDERER").is_err() {
+    //         std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    //     }
+    // }
 
     db_sync_lib::run();
 }
